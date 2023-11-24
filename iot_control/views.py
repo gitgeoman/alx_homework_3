be60c613011348eabbfc18d267fc17ff33092265
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-24 23:04:59 +0100

    wip: add models

--- iot_control/views.py
+++ iot_control/views.py
@@ -6,5 +6,10 @@ def landing_page(request):
     return render(
         request=request,
         template_name="iot_service/index.html",
-
     )
+
+
+
+def add(request):
+    if request.method != "POST":
+        return render(request, 'iot_service/add_device.html', {"form":ob})
