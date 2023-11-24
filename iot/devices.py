a07a8579a9cb8fbf9037b1acc6d15708a7e860e9
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2023-11-24 22:19:22 +0100

    wip: add other devices

--- iot/devices.py
+++ iot/devices.py
@@ -18,4 +18,47 @@ class Hue_light(Device):
         return 'Hue_light_status_ok'
 
 
+#Speaker
+class Remote_Speaker(Device):
+    
+    def connect(self):
+        print('Connecting to Remote Speaker')
+    
+    def disconnect(self):
+        print('Disconnectiong Remote Speaker')
+        
+    def send_message(self, message_type: MessageType, data:str)->None:
+        print(f'Remote Speaker handling message of type {message_type.name} with data [{data}]')
+        
+    def status_update(self):
+        return 'Remote_speaker_status_ok'
 
+#Music player 
+class Music_Player(Device):
+    
+    def connect(self):
+        print('Connecting to Music Player')
+    
+    def disconnect(self):
+        print('Disconnectiong Music Player')
+        
+    def send_message(self, message_type: MessageType, data:str)->None:
+        print(f'Music Player handling message of type {message_type.name} with data [{data}]')
+        
+    def status_update(self):
+        return 'Music Player_status_ok'
+    
+#Music player 
+class Rolety(Device):
+    
+    def connect(self):
+        print('Connecting to Rolety')
+    
+    def disconnect(self):
+        print('Disconnectiong Rolety')
+        
+    def send_message(self, message_type: MessageType, data:str)->None:
+        print(f'Rolety handling message of type {message_type.name} with data [{data}]')
+        
+    def status_update(self):
+        return 'Rolety_status_ok'
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from iot.device import Device
 from iot.service import IOTService
-from iot.devices import Hue_light
+from iot.devices import Hue_light, Remote_Speaker, Music_Player, Rolety
 from iot.message import Message,MessageType
 
 if __name__ == '__main__':
@@ -8,9 +9,19 @@ if __name__ == '__main__':
 
     # tworze instancje dostepnych urzadzeń
     hue_light = Hue_light()
-
-    # rejestruje urzadzenia
-    iot_service.register_device(dev=hue_light)
+    remote_speaker = Remote_Speaker()
+    music_player = Music_Player()
+    rolety = Rolety()
+    
+    available_devices: list[Device, Device] = [
+        hue_light,
+        remote_speaker,
+        music_player,
+        rolety,
+    ]
+    # rejestruje urzadzenia w iot_service
+    for device in available_devices:
+        iot_service.register_device(dev=device)
 
     # lista zarejestrowanych urzadzeń
     print(len(iot_service.devices))
@@ -19,15 +30,23 @@ if __name__ == '__main__':
     iot_service.test_device()
 
     # tworzy programy (listy Message)
-    lista_messagy =[
-        Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_ON),
-        Message(device_id=iot_service.devices[0].id, msg_type=MessageType.CHANGE_COLOR),
-        Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_OFF),
+    lista_messagy: dict = [
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_ON),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.CHANGE_COLOR),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_OFF),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_ON),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.CHANGE_COLOR),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_OFF),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_ON),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.PLAY_SONG),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.SWITCH_OFF),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.OPEN),
+            Message(device_id=iot_service.devices[0].id, msg_type=MessageType.CLOSE),
     ]
-
+    
     # uruchamiam programy
     iot_service.run_program(messages=lista_messagy)
-
+    
     # wyrejestrowanie urządzeń
     for device in iot_service.devices:
         iot_service.unregister_device(device.id)
@@ -37,3 +56,4 @@ if __name__ == '__main__':
         iot_service.get_device(device.id)
 
     # konczy program
+    
